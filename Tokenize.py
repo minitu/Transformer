4fa0acee25de30c085d7d66f3f2d37a3ad3cc8c8
Jaemin Choi <jchoi157@illinois.edu>
2019-03-23 17:00:43 -0400

    Remove trailing spaces and update indentation

--- Tokenize.py
+++ Tokenize.py
@@ -2,16 +2,15 @@ import spacy
 import re
 
 class tokenize(object):
-    
-    def __init__(self, lang):
-        self.nlp = spacy.load(lang)
-            
-    def tokenizer(self, sentence):
-        sentence = re.sub(
-        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(sentence))
-        sentence = re.sub(r"[ ]+", " ", sentence)
-        sentence = re.sub(r"\!+", "!", sentence)
-        sentence = re.sub(r"\,+", ",", sentence)
-        sentence = re.sub(r"\?+", "?", sentence)
-        sentence = sentence.lower()
-        return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
+  def __init__(self, lang):
+    self.nlp = spacy.load(lang)
+
+  def tokenizer(self, sentence):
+    sentence = re.sub(
+    r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(sentence))
+    sentence = re.sub(r"[ ]+", " ", sentence)
+    sentence = re.sub(r"\!+", "!", sentence)
+    sentence = re.sub(r"\,+", ",", sentence)
+    sentence = re.sub(r"\?+", "?", sentence)
+    sentence = sentence.lower()
+    return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
